6177be7774071cc3d822560792c7dcf3b18b3fe3
Germano Ribeiro <germanno.verde@gmail.com>
2024-05-06 19:27:16 -0300

    teste

--- src/theme/MyThemeProvider.tsx
+++ src/theme/MyThemeProvider.tsx
@@ -1,25 +1,14 @@
 import { ThemeProvider } from 'styled-components';
-import { dark , light } from '~/theme';
+import { dark  } from '~/theme';
 import GlobalStyle from './globalStyle';
-import { useState } from 'react';
+
 
 export const MyThemeProvider = ({ children }: any) => {
 
-
-  const [theme, setTheme] = useState('dark');
-
-  const toggleTheme = () => {
-    
-    setTheme(currentTheme => (currentTheme === 'dark'? 'light': 'dark'))
-
-
-
-  }
-   const selecionando = theme === 'dark'? dark: light; 
   return (
-    <ThemeProvider theme={selecionando}>
+    <ThemeProvider theme={dark}>
       <GlobalStyle /> 
-      <button onClick={toggleTheme}>Alternar Tema </button>
+    
        {children }
     </ThemeProvider>
 
