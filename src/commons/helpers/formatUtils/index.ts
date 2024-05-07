1f0d8b550e72d2147c65e49bab5d321cbfb58e17
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-05-07 09:44:05 -0300

    Feat:Added formatting functions for Cpf ,Date and currency

--- src/commons/helpers/formatUtils/index.ts
+++ src/commons/helpers/formatUtils/index.ts
@@ -21,3 +21,24 @@ export const formatPostalCode = (value: string) => {
   const formattedValue = value.replace(/\D/g, '').replace(/^(\d{5})(\d)/, '$1-$2');
   return formattedValue;
 };
+
+
+export const formatDate = (date: string) => {
+  const formattedDate = new Date(date).toLocaleDateString('pt-BR');
+  return formattedDate;
+}
+
+export const formatCPF = (value: string) => {
+  const formattedValue = value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{2})$/, '$1-$2');
+  return formattedValue;
+};
+
+export const formatCurrency = (value: number) => {
+  const formattedValue = value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  return formattedValue;
+};
+
