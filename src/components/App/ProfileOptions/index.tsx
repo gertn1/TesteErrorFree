fb64875abad16b3c4a5a3d572cbf22c605b1f814
Germano Ribeiro <germanno.verde@gmail.com>
2024-05-09 19:32:12 -0300

    testes para mudar o tema

--- src/components/App/ProfileOptions/index.tsx
+++ src/components/App/ProfileOptions/index.tsx
@@ -1,6 +1,39 @@
+// import { Switch } from 'antd';
+// import styled from 'styled-components';
+
+// import {
+//   BorderGoOut,
+//   ContainerProfileOptions,
+//   ContentConfig,
+//   ContentTheme,
+//   GoOut,
+// } from '~/components/App/ProfileOptions/style.ts';
+
+// const StyledSwitch = styled(Switch)`
+//   margin-left: 100px;
+//   transform: scale(0.7);
+  
+// `;
+
+// export const ProfileOptions = () => {
+//   return (
+//     <ContainerProfileOptions>
+//       <ContentConfig>Configurações do Perfil</ContentConfig>
+//       <ContentTheme>
+//         Dark Theme
+//         <StyledSwitch />
+//       </ContentTheme>
+
+//       <BorderGoOut>
+//         <GoOut>Sair</GoOut>
+//       </BorderGoOut>
+//     </ContainerProfileOptions>
+//   );
+// };
+
+import React from 'react';
 import { Switch } from 'antd';
 import styled from 'styled-components';
-
 import {
   BorderGoOut,
   ContainerProfileOptions,
@@ -9,18 +42,20 @@ import {
   GoOut,
 } from '~/components/App/ProfileOptions/style.ts';
 
+// Defina o componente estilizado
 const StyledSwitch = styled(Switch)`
   margin-left: 100px;
   transform: scale(0.7);
 `;
 
-export const ProfileOptions = () => {
+export const ProfileOptions = ({ toggleTheme }: { toggleTheme: () => void }) => {
   return (
     <ContainerProfileOptions>
       <ContentConfig>Configurações do Perfil</ContentConfig>
       <ContentTheme>
         Dark Theme
-        <StyledSwitch />
+        {/* Use o StyledSwitch e adicione um evento onChange para alternar o tema */}
+        <StyledSwitch onChange={toggleTheme} />
       </ContentTheme>
 
       <BorderGoOut>
--- src/theme/MyThemeProvider.tsx
+++ src/theme/MyThemeProvider.tsx
@@ -1,16 +1,40 @@
-import { ThemeProvider } from 'styled-components';
-import { dark  } from '~/theme';
-import GlobalStyle from './globalStyle';
+// import { ThemeProvider } from 'styled-components';
+// import { dark, light  } from '~/theme';
+// import GlobalStyle from './globalStyle';
 
+
+// export const MyThemeProvider = ({ children }: any) => {
+
+//   return (
+//     <ThemeProvider theme={light}>
+//       <GlobalStyle /> 
+      
+//        {children }
+//     </ThemeProvider>
+
+//   );
+// };
+
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { dark, light } from '~/theme';
+import GlobalStyle from './globalStyle';
+import { ProfileOptions } from '../components/App/ProfileOptions'; // Importe o componente ProfileOptions
 
 export const MyThemeProvider = ({ children }: any) => {
+  const [theme, setTheme] = useState(light); // Inicializa o tema como light
+
+  // Função para alternar entre os temas
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === dark ? light : dark));
+  };
 
   return (
-    <ThemeProvider theme={dark}>
-      <GlobalStyle /> 
-    
-       {children }
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {/* Passe a função toggleTheme para o ProfileOptions */}
+      <ProfileOptions toggleTheme={toggleTheme} />
+      {children}
     </ThemeProvider>
-
   );
 };
